8d53dd4d730bac4c7a6854c7e18f5bf041e76872
Pablo Guerrero <pablo.guerrero-gomez@ext.ec.europa.eu>
2024-05-05 16:22:53 +0200

    usar gradle en lugar de maven como build tool

--- src/test/java/com/nacho/inmuebles/ApplicationTests.java
+++ src/test/java/com/nacho/inmuebles/ApplicationTests.java
@@ -1,13 +1,19 @@
 package com.nacho.inmuebles;
 
+import com.nacho.inmuebles.controller.HomeController;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 @SpringBootTest
 class ApplicationTests {
+	@Autowired
+	private HomeController controller;
 
 	@Test
 	void contextLoads() {
+		assertThat(controller).isNotNull();
 	}
-
 }
